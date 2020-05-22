29cf10edc335e1e5e6bb559e906e04d7a07d629a
Ron Burk <RonBurk@peoplewideweb.org>
2020-05-22 15:53:57 -0700

    Possibly got a module to compile.

--- v8main.cc
+++ v8main.cc
@@ -10,6 +10,26 @@
 #include "libplatform/libplatform.h"
 #include "v8.h"
 
+using v8::Local;
+using v8::ScriptOrigin;
+using v8::Isolate;
+
+Local<v8::String> NewV8Str(Isolate* isolate, const char* src)
+    {
+    Local<v8::String> result =
+        v8::String::NewFromUtf8(isolate, "import * from './main.mjs';",
+            v8::NewStringType::kNormal)
+        .ToLocalChecked();
+    return result;
+    }
+
+ScriptOrigin ModuleOrigin(Local<v8::Value> resource_name, Isolate* isolate) {
+  ScriptOrigin origin(resource_name, Local<v8::Integer>(), Local<v8::Integer>(),
+                      Local<v8::Boolean>(), Local<v8::Integer>(),
+                      Local<v8::Value>(), Local<v8::Boolean>(),
+                      Local<v8::Boolean>(), True(isolate));
+  return origin;
+}
 void V8Main(int ArgCount, char** Args)
     {
     // Initialize V8.
@@ -32,17 +52,31 @@ void V8Main(int ArgCount, char** Args)
         v8::HandleScope handle_scope(isolate);
 
         // Create a new context.
-        v8::Local<v8::Context> context = v8::Context::New(isolate);
+        Local<v8::Context> context = v8::Context::New(isolate);
 
         // Enter the context for compiling and running the hello world script.
         v8::Context::Scope context_scope(context);
             {
             // Create a string containing the JavaScript source code.
-            v8::Local<v8::String> source =
-                v8::String::NewFromUtf8(isolate, "import * from './main.mjs';",
+            Local<v8::String> source =
+                v8::String::NewFromUtf8(isolate, "import './main.mjs';",
                     v8::NewStringType::kNormal)
                 .ToLocalChecked();
+            Local<v8::String> modname=NewV8Str(isolate, "root_module");
+            
 
+            ScriptOrigin origin = ModuleOrigin(modname, isolate);
+            v8::ScriptCompiler::Source modsource(source,origin);
+            Local<v8::Module> module;
+            if(v8::ScriptCompiler::CompileModule(isolate, &modsource).ToLocal(&module))
+                {
+                printf("compile module worked.\n");
+                }
+            else
+                {
+                printf("compile module failed.\n");
+                }
+#if 0
             // Compile the source code.
             v8::Local<v8::Script> script;
             if(v8::Script::Compile(context, source).ToLocal(&script))
@@ -62,6 +96,7 @@ void V8Main(int ArgCount, char** Args)
             // Convert the result to an UTF8 string and print it.
             v8::String::Utf8Value utf8(isolate, result);
             printf("%s\n", *utf8);
+#endif
             }
 
         }
