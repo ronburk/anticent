bca1114fd44c06d52f26fdf41dbc9e985d0747b6
Ron Burk <RonBurk@peoplewideweb.org>
2020-05-22 01:40:57 -0700

    header file for common decls

---
+++ common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+/* common enough constructs for most files to include */
+
+#include <memory>
+using std::shared_ptr;
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include <cassert>
+
+
+#endif /* COMMON_H_ */
