8a00468f627c62b8fe5e5e728db1767558daa05f
Ron Burk <RonBurk@peoplewideweb.org>
2020-05-22 01:41:34 -0700

    fudge

--- process.cc
+++ process.cc
@@ -1,12 +1,35 @@
+/* process.cc - implement Process, a context in which a given V8 Task runs.
+
+Every V8 Task belongs to (holds a shared_ptr to) a Process. A Process is analogous
+to a *nix process, in that it provides context for code when it is running.
+
+
+ */
+
 #include "process.h"
 using std::make_shared;
 
-Process::Process(int uid, int layer, std::string cwd)
-    : uid(uid), layer(layer), cwd(cwd)
+namespace
     {
+    int                         nextProcId = 0;
+    vector<shared_ptr<Process>> procList(10);
     }
 
+
+Process::Process(int uid, int layer, std::string cwd, int parentProcID)
+    : uid(uid), layer(layer), cwd(cwd), parentProcID(parentProcID)
+    {
+    procid = nextProcId++;   // assign this process the next available process ID
+    // ???TODO handle wraparound of procid
+    assert(procid < 9999);
+    }
+
+/* New() - return shared pointer to new Process.
+ */
 shared_ptr<Process> Process::New(int uid, int layer, string cwd)
     {
-    return make_shared<Process>(uid, layer, cwd);
+    auto    Result = make_shared<Process>(uid, layer, cwd);
+
+    procList.push_back(Result);
+    return Result;
     }
--- process.h
+++ process.h
@@ -1,21 +1,22 @@
 #ifndef PROCESS_H_
 #define PROCESS_H_
 
-#include <string>
-using std::string;
-#include <memory>
-using std::shared_ptr;
+#ifndef COMMON_H_
+#include "common.h"
+#endif
+
 
 class Process
     {
 public:
-    Process(int uid, int layer, std::string cwd);
+    Process(int uid, int layer, std::string cwd, int parentProcid=0);
     virtual ~Process(){}
     int         procid;  // ID of this process
     int         uid;     // ID of user owning this process
 //    int     gid;   // let's not do groups just yet
     int         layer;   // ID of resource identifying active layer (0 for base layer)
     string      cwd;
+    int         parentProcID;
     static shared_ptr<Process> New(int uid, int layer, string cwd);
 protected:
     };
