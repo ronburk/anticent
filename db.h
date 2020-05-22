424d790a8a75470ddc87afee80ca105b6caf81c2
Ron Burk <RonBurk@peoplewideweb.org>
2020-05-22 00:05:33 -0700

    skeleton db files

---
+++ db.h
@@ -0,0 +1,18 @@
+#ifndef DB_H_
+#define DB_H_
+
+/* db.h - abstract interface to database.
+ *
+ * uid, gid, res, resLayer
+ */
+
+
+class Database
+    {
+public:
+    Database();
+   ~Database();
+//    char* Read(
+    };
+
+#endif /* DB_H_ */
