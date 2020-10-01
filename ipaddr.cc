f7afcfeacf1a7141c3891b6b21635a12a3f9e32a
Ron Burk <ronburk@gmail.com>
2020-09-30 23:15:44 -0700

    rename

---
+++ ipaddr.cc
@@ -0,0 +1,176 @@
+#include "ipaddr.h"
+#include <cassert>
+#include <ifaddrs.h>
+#include <netdb.h>
+
+// little helper function to explain errno and die
+void    DieIf(bool failed, const char* name)
+    {
+    if(failed)
+        {
+        string msg(name);
+        msg     += " failed.";
+        perror(msg.c_str());
+        assert(false);
+        }
+    }
+
+void IPaddr::SetPort(int port)
+    {
+    ip4.sin_port = htons(port);
+    }
+
+void IPaddr::SetFromSockaddr(struct sockaddr* other)
+    {
+    memcpy(&any, other, operator socklen_t());
+    }
+
+IPaddr::IPaddr(bool IP6)
+    {
+    memset(&any, 0, sizeof(any));
+    any.sa_family = IP6 ? AF_INET6 : AF_INET;
+    }
+
+fd_t IPaddr::Accept(fd_t listenFd)
+    {
+    fd_t    result;
+    IPaddr  newAddr(true);
+
+    socklen_t size = newAddr;
+    result = accept4(listenFd, newAddr, &size, SOCK_NONBLOCK);
+    DieIf(result < 0, "accept4");
+    return result;
+    }
+
+
+void IPaddr::SetAddrFromEth(string ethname)
+    {
+    struct ifaddrs*     listhead;
+    int status = getifaddrs(&listhead);
+    DieIf(status != 0, "getifaddrs");
+
+    bool found = false;
+    for(auto list=listhead; list; list = list->ifa_next)
+        if(list->ifa_name == ethname && list->ifa_addr) // ifa_addr sometimes null...
+            if(list->ifa_addr->sa_family==GetFamily())
+                {
+                SetFromSockaddr(list->ifa_addr);
+                found           = true;
+                }
+    freeifaddrs(listhead);
+    if(!found)
+        {
+        fprintf(stderr, "SetAddrFromEth() could not find interface '%s'\n",
+            ethname.c_str());
+        assert(false);
+        }
+    }
+
+
+string mygetname(const struct sockaddr* addr, int flags)
+    {
+    char        output[256];
+    socklen_t socklen;
+    switch(addr->sa_family)
+        {
+        case AF_INET6   : socklen = sizeof(struct sockaddr_in6);    break;
+        case AF_INET    : socklen = sizeof(struct sockaddr_in);     break;
+        default: assert(false);
+        }
+
+    int     status = getnameinfo(addr, socklen, output, sizeof(output)-1, nullptr, 0, flags);
+    const char* errtext = nullptr;
+    switch(status)
+        {
+        case    EAI_AGAIN:
+            errtext = "EAI_AGEIN: name could not be resolved; try again later";
+            break;
+        case    EAI_BADFLAGS:
+            errtext = "EAI_BADFLAGS: invalid 'flags' argument";
+            break;
+        case    EAI_FAIL:
+            errtext = "EAI_FAIL: nonrecoverable error occurred";
+            break;
+        case    EAI_FAMILY:
+            errtext = "EAI_FAMILY: address family unknown or address length wrong for family";
+            break;
+        case    EAI_MEMORY:
+            errtext = "EAI_MEMORY: out of memory";
+            break;
+        case    EAI_NONAME:
+            errtext = "EAI_NONAME: NI_NAMEREQD is set and host's name can't be located, or neither host/service name was requested";
+            break;
+        case    EAI_OVERFLOW:
+            errtext = "EAI_OVERFLOW: output buffer too small";
+            break;
+        case    EAI_SYSTEM:
+            DieIf(true, "getnameinfo");
+            break;
+        }
+    if(errtext)
+        {
+        fprintf(stderr, "getnameinfo() failed: '%s'\n", errtext);
+        assert(false);
+        }
+    return output;
+    }
+
+fd_t IPaddr::Bind(const string& nicname, int port)
+    {
+    SetAddrFromEth(nicname);
+    fd_t listenFd = socket(GetFamily(), SOCK_STREAM|SOCK_NONBLOCK, 0);
+    DieIf(listenFd < 0, "socket");
+    SetPort(port);
+    int status = bind(listenFd, *this, *this); // conv operators know what to do
+    DieIf(status != 0, "bind");
+    status = listen(listenFd, 100);
+    DieIf(status != 0, "listen");
+
+    return listenFd;
+    }
+
+
+#if 0
+struct sockaddr* EthAddrFromName(const string& name, bool Ip6)
+    {
+    static struct sockaddr ImGoingToHell;
+    struct ifaddrs*     listhead;
+    int status = getifaddrs(&listhead);
+    DieIf(status != 0, "getifaddrs");
+
+    bool found = false;
+    for(auto list=listhead; list; list = list->ifa_next)
+        if(list->ifa_name == name && list->ifa_addr)
+            {
+//            char    dumpstr[256] = "";
+            int     family = list->ifa_addr->sa_family;
+            if(family==AF_INET6 || family==AF_INET)
+                {
+                auto addrstr = mygetname(list->ifa_addr, NI_NUMERICHOST);
+                fprintf(stderr, "Interface '%s', IP%s addr='%s'\n", name.c_str(),
+                    (family==AF_INET6)?"6":"", addrstr.c_str());
+                if((Ip6 && (family==AF_INET6)) || (family == AF_INET))
+                    {
+                    ImGoingToHell   = *list->ifa_addr;
+                    found           = true;
+                    }
+                }
+            else if(family == AF_PACKET) // sometimes called AF_LINK?
+                ;   // could print MAC address I guess
+            else    // else let's notice if the possibilities are different than we imagine
+                {
+                fprintf(stderr, "what is family %d on %s?\n", list->ifa_addr->sa_family, name.c_str());
+                assert(false);
+                }
+            }
+    freeifaddrs(listhead);
+    if(found)
+        return &ImGoingToHell;
+    fprintf(stderr, "EthAddrFromName() could not find interface '%s'\n",
+        name.c_str());
+    assert(false);
+    }
+
+
+
+#endif
---
+++ ipaddr.h
@@ -0,0 +1,48 @@
+#ifndef IPADDR_H_
+#define IPADDR_H_
+
+/* ipaddr.h - define TCP sockets
+ *
+ * Main point is to hide the tedious distinguishing between
+ * IPv4 and IPv6 socket structures. Note that IPaddr only holds
+ * a socket structure, not a file descriptor. Don't want to force
+ * keeping around this info for 10,000 open connections, especially
+ * since it can be retrieved via the file descriptor by getsockname().
+ */
+
+#include "poll.h"
+#include <string>
+using std::string;
+
+#include <sys/socket.h>
+//#include <sys/types.h>
+#include <arpa/inet.h>
+
+class   IPaddr
+    {
+    union
+        {
+        struct sockaddr     any;
+        struct sockaddr_in  ip4;
+        struct sockaddr_in6 ip6;
+        };
+public:
+    static fd_t Accept(fd_t listenFd);
+    IPaddr(bool IP6);
+    bool IsIP6() { return any.sa_family == AF_INET6; }
+    int  GetFamily() { return any.sa_family; }
+    void SetFromSockaddr(struct sockaddr* other);
+    void SetPort(int port);
+    operator socklen_t()
+        {
+        return any.sa_family == AF_INET ?
+            sizeof(struct sockaddr_in)
+            : sizeof(struct sockaddr_in6);
+        }
+    operator struct sockaddr*() { return &any;    }
+    void SetAddrFromEth(string ethname);
+//    fd_t Accept(fd_t listenFd);
+    fd_t Bind(const string& nicname, int port);
+    };
+
+#endif /*  IPADDR_H_ */
